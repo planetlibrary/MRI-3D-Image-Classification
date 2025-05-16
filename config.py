0b7d4ea6418ecb4c5cd162e9517b099f2901254d
planetlibrary <sayantan.15biswas@gmail.com>
2025-05-16 12:59:00 +0530

    add parameters

--- config.py
+++ config.py
@@ -2,42 +2,87 @@
 
 import os
 from pathlib import Path
+import torch
 
 class Config:
-    # Paths
-    data_dir = Path("./data/3D")
-    train_dir = data_dir / "Train"
-    val_dir = data_dir / "Val"
-    test_dir = data_dir / "Test"
-    output_dir = Path("./outputs")
-    checkpoints_dir = output_dir / "checkpoints"
-    logs_dir = output_dir / "logs"
-    metrics_dir = output_dir / "metrics"
+    def __init__(self, model_name= 'ViTForClassification'):
+        self.model_name = model_name
 
-    # Training Parameters
-    num_epochs = 50
-    batch_size = 8
-    learning_rate = 1e-4
-    weight_decay = 1e-5
-    scheduler_step = 10
-    scheduler_gamma = 0.1
-    save_every_n_epochs = 10
+        # Paths
+        self.data_dir = Path("./data/3D")
+        self.train_dir =self.data_dir / "Train"
+        self.val_dir = self.data_dir/ "Val"
+        self.test_dir = self.data_dir/ "Test"
+        self.output_dir = Path("./outputs")
+        self.checkpoints_dir =self.output_dir /self.model_name/ "checkpoints"
+        self.logs_dir = self.output_dir / self.model_name/"logs"
+        self.metrics_dir = self.output_dir/ self.model_name/"metrics"
+        self.figures_dir = self.output_dir/ self.model_name/"figures"
 
-    # Model
-    project_name = "ViT-Nicara-3D"   # Name of model class in models.py
-    model_name = "ViT3D"
-    num_classes = 3              # AD, CN, MCI
-    input_channels = 1          # Adjust based on your 3D input (e.g., grayscale)
-    input_channels = 1
-    img_size = 64
-    patch_size = 16
-    emb_dim = 128
-    num_heads = 4
-    num_layers = 6
-    dropout = 0.1
-    num_classes = 3
+        # Training Parameters
+        self.num_epochs = 50
+        self.batch_size = 8
+        self.learning_rate = 1e-4
+        self.weight_decay = 2e-3
+        self.scheduler_step = 10
+        self.scheduler_gamma = 0.1
 
-    # System
-    device = "cuda" if os.environ.get("CUDA_VISIBLE_DEVICES", "") else "cpu"
-    num_workers = 4
-    seed = 42
+        
+        self.save_every_n_epochs = 1
+
+        # Model
+        self.project_name = "ViT-Nicara-3D"   # Name of model class in models.py
+
+        if self.model_name == "ViTForClassfication_V2":
+            self.num_classes = 3              # AD, CN, MCI
+            self.input_channels = 1          # Adjust based on your 3D input (e.g., grayscale)
+            self.image_size = 192
+            self.patch_size = 16
+            self.emb_dim = 128
+            self.num_heads = 4
+            self.num_layers = 6
+            self.hidden_size = 27
+            self.dropout = 0.1
+
+        elif self.model_name == "ViT3D_V1":
+            self.num_classes = 3              # AD, CN, MCI
+            self.input_channels = 1          # Adjust based on your 3D input (e.g., grayscale)
+            self.image_size = 192
+            self.patch_size = 16
+            self.emb_dim = 128
+            self.num_heads = 4
+            self.num_layers = 6
+            self.hidden_size = 27
+            self.dropout = 0.1
+
+
+        elif self.model_name == "ViTForClassification":
+            self.batch_size = 5 #3
+            self.image_size = 192
+            self.patch_size = 6
+            self.hidden_size= 27 #216
+            self.num_hidden_layers= 2 #3
+            self.num_attention_heads= 4 #8
+            self.intermediate_size= 256 # 3 * 216 # 3 * hidden_size
+            self.hidden_dropout_prob= 0.2 # 0.25
+            self.attention_probs_dropout_prob= 0.2 #0.25
+            self.initializer_range= 0.02
+            self.num_classes= 3 # num_classes
+            self.num_channels= 1
+            self.qkv_bias= True
+            self.use_faster_attention= True
+            self.lr = 5e-5
+            # save_model_every = 10
+            # exp_name = '3D ViT Final'
+            # model_name = 'Hybrid'
+            # epochs = 150
+
+        else:
+            raise KeyError("Model name do not exist in the existing list: ['ViTForClassfication_V2', 'ViT3D_V1', 'ViTForClassification']")
+
+
+
+        # System
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.num_workers = 4
+        self.seed = 42
